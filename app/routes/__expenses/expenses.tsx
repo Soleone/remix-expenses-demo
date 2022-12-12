a9fd9640094722f4fa0186b706e1413d1df15bb2
Soleone <dev@soleone.xyz>
2022-12-12 02:00:12 -0500

    Expense actions with icons

--- app/routes/__expenses/expenses.tsx
+++ app/routes/__expenses/expenses.tsx
@@ -1,8 +1,9 @@
 import type { LinksFunction } from "@remix-run/node"
-import { Outlet } from "@remix-run/react"
+import { Link, Outlet } from "@remix-run/react"
 import expensesStyles from "~/styles/expenses.css"
 import { expenses } from "~/model/expenses"
 import ExpensesList from "~/components/expenses/ExpensesList"
+import { FaDownload, FaPlus } from "react-icons/fa"
 
 export const links: LinksFunction = () => {
   return [
@@ -15,6 +16,16 @@ export default function ExpensesLayout() {
     <>
       <Outlet />
       <main>
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add expense</span>
+          </Link>
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Download</span>
+          </a>
+        </section>
         <ExpensesList expenses={expenses} />
       </main>
     </>
