a435c6d6e87074cd2e930a64839ee4a69826db14
Soleone <dev@soleone.xyz>
2022-12-12 01:52:51 -0500

    url search params toggle between signup and login

--- app/components/auth/AuthForm.tsx
+++ app/components/auth/AuthForm.tsx
@@ -1,7 +1,14 @@
-import { Link } from '@remix-run/react'
+import { Link, useSearchParams } from '@remix-run/react'
 import { FaLock } from 'react-icons/fa'
+import { AUTH_MODE_PARAM_NAME, LOGIN_MODE, SIGNUP_MODE } from '~/model/constants'
 
 export default function AuthForm() {
+  const [searchParams] = useSearchParams()
+  const mode = searchParams.get(AUTH_MODE_PARAM_NAME) || LOGIN_MODE
+  const submitButtonLabel = mode === LOGIN_MODE ? "Log in" : "Sign up"
+  const toggleButtonLabel = mode === LOGIN_MODE ? "Sign up as new user" : "Log in with existing user"
+  const toggleToMode = mode === LOGIN_MODE ? SIGNUP_MODE : LOGIN_MODE
+
   return (
     <form method="post" className="form" id="auth-form">
       <div className="icon-img">
@@ -16,8 +23,8 @@ export default function AuthForm() {
         <input type="password" id="password" name="password" minLength={7} />
       </p>
       <div className="form-actions">
-        <button>Login</button>
-        <Link to="/auth">Log in with existing user</Link>
+        <button>{submitButtonLabel}</button>
+        <Link to={`?${AUTH_MODE_PARAM_NAME}=${toggleToMode}`}>{toggleButtonLabel}</Link>
       </div>
     </form>
   )
---
+++ app/model/constants.ts
@@ -0,0 +1,3 @@
+export const AUTH_MODE_PARAM_NAME = "mode"
+export const SIGNUP_MODE = "signup"
+export const LOGIN_MODE = "login"
