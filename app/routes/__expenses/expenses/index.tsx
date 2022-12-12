45dcdc78ef634db9f2234ed1596288dc3a4a032a
Soleone <dev@soleone.xyz>
2022-12-12 00:52:20 -0500

    Rename __app to __expenses

--- app/routes/__expenses/expenses/index.tsx
+++ app/routes/__expenses/expenses/index.tsx
@@ -1,11 +1,7 @@
 import type { MetaFunction } from "@remix-run/node"
 
 export default function ExpensesRoute() {
-  return (
-    <div>
-      <h1>All Expenses</h1>
-    </div>
-  )
+  return <></>
 }
 
 export const meta: MetaFunction = () => {
